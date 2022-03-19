c07f49251e25392c4a622ba7c54a27f4345a259b
Brandon Yates <brandonyates@usf.edu>
2022-03-18 21:41:51 -0400

    detects if hazard is inside or outside the ROI

---
+++ src/detectHazardInOutROI.go
@@ -0,0 +1,18 @@
+//check if user checked inside or outside roi
+
+package main
+
+//"fmt"
+//"log"
+//"net/http"
+
+func detectHazardInOutROI() {
+	//get information from front end
+
+	//getROI()
+
+	//if user selected inside box and hazard is in box:
+	//send an SMS
+	//else if user selected outside box and hazard is detected and outside box:
+	//send SMS
+}
