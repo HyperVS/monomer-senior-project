3fb8203cbbe50031215b73391a3173f8376db8d2
Brandon <45293962+brandyates@users.noreply.github.com>
2022-04-18 23:00:18 -0400

    Updated field name for models.go to be more clear

--- Backend/roi/models/models.go
+++ Backend/roi/models/models.go
@@ -5,11 +5,11 @@ package models
 //Location: User selected if they want to alert from inside or outside the ROI.
 //Location will be either "inside" or "outside"
 //Data: string containing the points of the ROI in the format "Top: y, Left: x, Width: w, Height: h"
-//Select: string of what the user wants to detect.
-//Select will equal either "hardhat" or "no_hardhat"
+//Detect: string of what the user wants to detect.
+//Detect will be either "hardhat" or "no_hardhat"
 type Models struct {
 	ID       uint   `gorm:"primary_key;auto_increment;not_null"`
 	Location string `json:"location"`
 	Data     string `json:"data"`
-	Select   string `json:"select"`
+	Detect   string `json:"detect"`
 }
